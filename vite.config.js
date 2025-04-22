958be47b00baf109fdb38a2c68849e3b3735bc90
Vaibhav9313 <vaibhavjadhav9313@gmail.com>
2025-04-22 10:03:36 +0530

    Updated dependencies and configuration

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('vite').UserConfig} */
 export default {
   plugins: [svelte()],
+  server: {
+    // hmr: true,  // No need to manually set this
+  },
 };
