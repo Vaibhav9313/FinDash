59117068d9839de8e666f9a14d7e96e6e92efaea
Vaibhav9313 <vaibhavjadhav9313@gmail.com>
2025-04-22 14:56:39 +0530

    Dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,22 @@
 import sveltePreprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
   preprocess: [vitePreprocess(), sveltePreprocess()],
+
+  kit: {
+    // Specify the adapter to generate static assets
+    adapter: adapter(),
+
+    // You can specify other kit options here as needed
+    paths: {
+      base: '', // Specify your base path if needed (for example, if the app is hosted in a subfolder)
+    },
+
+    // Enable prerendering if necessary
+    prerender: {
+      default: true, // Prerender all pages by default
+    },
+  },
 };
